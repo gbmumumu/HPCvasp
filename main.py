fe44763e43d813e9c4271a2d0e66d4f77641503e
SenGao <sen.gaobo@gmail.com>
2021-06-01 14:48:54 +0800

    update yhrum

--- main.py
+++ main.py
@@ -3,12 +3,8 @@
 
 
 if __name__ == '__main__':
-    from time import sleep
     from utils.spath import SPath
-    from hpc import yhurm
-    from utils.tools import LogCsv
-    from calculation.vasp.errors.eskill import ESkill
-    from calculation.vasp.inputs import INCAR
+    from utils import yhurm
     from calculation.vasp.inputs import POSCAR
 
     v = yhurm.TianHeJob(job_id=17123506).get_time()
